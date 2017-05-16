3f65aee884dd5a7027e177d0ddeab8021ed0cecf
Maxime Rainville <maxime@syrp.co.nz>
2017-05-16 17:55:18 +1200

    Add a proxy response type.

--- src/Types.ts
+++ src/Types.ts
@@ -1,3 +1,12 @@
+import { IncomingMessage } from 'http';
+import { Buffer } from 'buffer';
+
+
 export type Map<T> = {[key: string]: T};
 
 export type HttpVerb = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS' | 'PATCH';
+
+export interface ProxyResponse {
+    message: IncomingMessage,
+    body: string | Buffer
+}
